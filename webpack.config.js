c1df5d227f8f04a8d814724f70131cc049fdc2c7
nesacar <nebojsart1409@yahoo.com>
2018-01-05 20:09:15 +0100

    jquery plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ var path = require('path');
 
 var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var webpack = require('webpack');
 
 var extractPlugin = new ExtractTextPlugin({
     filename: 'main.css'
@@ -12,7 +15,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/dist'
+        //publicPath: '/dist'
     },
     module: {
         rules: [
@@ -32,13 +35,57 @@ module.exports = {
                 use: extractPlugin.extract({
                     use: ['css-loader', 'sass-loader']
                 })
+            },
+            /*{
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: '/',
+                        }
+                    }
+                ],
+                exclude: path.resolve(__dirname, 'src/index.html')
+            },*/
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png|jpeg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            //publicPath: 'img/'
+                        }
+                    }
+                ]
             }
         ]
     },
     plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
         new UglifyJsPlugin({
             //...
         }),
-        extractPlugin
+        extractPlugin,
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'users.html',
+            template: 'src/users.html',
+            chunks: []
+        }),
+        new CleanWebpackPlugin(['dist'])
     ]
 };
