d4702d5af6751b83a15347e39df0f3a3783d7eb7
nesacar <nebojsart1409@yahoo.com>
2018-01-05 16:25:07 +0100

    add features

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 var path = require('path');
+
 var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var extractPlugin = new ExtractTextPlugin({
+    filename: 'main.css'
+});
 
 module.exports = {
     entry: './src/js/app.js',
@@ -11,17 +17,28 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/,
+                test: /\.js$/,
                 use: [
-                    'style-loader',
-                    'css-loader'
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
                 ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                    use: ['css-loader', 'sass-loader']
+                })
             }
         ]
     },
     plugins: [
         new UglifyJsPlugin({
             //...
-        })
+        }),
+        extractPlugin
     ]
 };
